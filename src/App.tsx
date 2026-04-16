d791bab97684036615889aaa599f9df9944eb472
hmmrfll <a.kazakevych@gmail.com>
2026-04-16 13:56:24 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -24,26 +24,24 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <main className="app-main">
-        <Hero onNavigate={handleNavigate} />
+      <Hero onNavigate={handleNavigate} />
 
-        <HowItWorks
-          sectionRef={(el) => {
-            if (el) sectionsRef.current['how-it-works'] = el as HTMLDivElement
-          }}
-          onNavigate={handleNavigate}
-          isVisible={isVisible}
-          shouldShowOnboarding={shouldShowOnboarding}
-          onOnboardingShown={() => setShouldShowOnboarding(false)}
-        />
+      <HowItWorks
+        sectionRef={(el) => {
+          if (el) sectionsRef.current['how-it-works'] = el as HTMLDivElement
+        }}
+        onNavigate={handleNavigate}
+        isVisible={isVisible}
+        shouldShowOnboarding={shouldShowOnboarding}
+        onOnboardingShown={() => setShouldShowOnboarding(false)}
+      />
 
-        <WhyUs
-          sectionRef={(el) => {
-            if (el) sectionsRef.current['why-us'] = el as HTMLDivElement
-          }}
-          isVisible={isVisible}
-        />
-      </main>
+      <WhyUs
+        sectionRef={(el) => {
+          if (el) sectionsRef.current['why-us'] = el as HTMLDivElement
+        }}
+        isVisible={isVisible}
+      />
 
       <Footer />
     </div>
