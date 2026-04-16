593d505e4993161ff7fb484fa83f05e2bc8f2460
hmmrfll <a.kazakevych@gmail.com>
2026-04-16 13:54:57 +0300

    Refactor app layout and styling: update CSS for app structure, change background color, and wrap main content in a new main element.

--- src/App.tsx
+++ src/App.tsx
@@ -24,28 +24,29 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <main className="app-main">
+        <Hero onNavigate={handleNavigate} />
 
-      <Hero onNavigate={handleNavigate} />
+        <HowItWorks
+          sectionRef={(el) => {
+            if (el) sectionsRef.current['how-it-works'] = el as HTMLDivElement
+          }}
+          onNavigate={handleNavigate}
+          isVisible={isVisible}
+          shouldShowOnboarding={shouldShowOnboarding}
+          onOnboardingShown={() => setShouldShowOnboarding(false)}
+        />
 
-      <HowItWorks
-        sectionRef={(el) => {
-          if (el) sectionsRef.current['how-it-works'] = el as HTMLDivElement
-        }}
-        onNavigate={handleNavigate}
-        isVisible={isVisible}
-        shouldShowOnboarding={shouldShowOnboarding}
-        onOnboardingShown={() => setShouldShowOnboarding(false)}
-      />
-
-      <WhyUs
-        sectionRef={(el) => {
-          if (el) sectionsRef.current['why-us'] = el as HTMLDivElement
-        }}
-        isVisible={isVisible}
-      />
+        <WhyUs
+          sectionRef={(el) => {
+            if (el) sectionsRef.current['why-us'] = el as HTMLDivElement
+          }}
+          isVisible={isVisible}
+        />
+      </main>
 
       <Footer />
-      </div>
+    </div>
   )
 }
 
